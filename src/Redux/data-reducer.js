1b4a41460c07917d9827da650e1a9ca56fc4603c
Ksdem <ksdem9488@gmail.com>
2020-09-18 17:06:25 +0300

    stateCopy

--- src/Redux/data-reducer.js
+++ src/Redux/data-reducer.js
@@ -41,14 +41,18 @@ const dataReducer = (state= initialState, action) => {
 
 
     switch (action.type) {
-        case UPDATE_NEW_MESSAGE_BODY:
-            state.newMessageBody = action.body;
-            return state;
-        case  SEND_MESSAGE:
-            let body = state.newMessageBody;
-            state.newMessageBody = '';
-            state.messagesData.push({id: 6, message: body});
-            return state;
+        case UPDATE_NEW_MESSAGE_BODY:{
+            let stateCopy={...state};
+            stateCopy.newMessageBody=[...state.newMessageBody];
+            stateCopy.newPostText='';
+            stateCopy.newMessageBody = action.body;
+            return stateCopy;}
+        case  SEND_MESSAGE:{
+            let stateCopy={...state}
+            let body = stateCopy.newMessageBody;
+            stateCopy.newMessageBody = '';
+            stateCopy.messagesData.push({id: 6, message: body});
+            return stateCopy;}
         default:
             return state;
     }
